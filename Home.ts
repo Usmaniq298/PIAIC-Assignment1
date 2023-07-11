4e99bbe8f9446040272ba24af0aa0fa6726dc7ca
Usmaniq298 <mian.usmaniqbal@gmail.com>
2023-07-11 17:47:32 +0500

    some more chnages

--- Home.ts
+++ Home.ts
@@ -2,7 +2,7 @@
 console.log("Hello");
 
 // Point 2
-var PersonName:string = "Usman Iqbal";
+var PersonName : string = "Usman Iqbal";
 console.log("How Are you " , PersonName  , "? would you like to learn some Python today?");
 
 //Point 3 Bys use the same varibale for person name as use in point 2 
@@ -12,6 +12,6 @@ console.log("To Upper Case: " ,PersonName.toUpperCase() , " To Lower case: ", Pe
 console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
 
 // Point 5 
-var famous_person:string = "Usman Iqbal";
+var famous_person : string = "Usman Iqbal";
 var message : string = "“A person who never made a mistake never tried anything new.”";
 console.log (famous_person , "once said," , message);
